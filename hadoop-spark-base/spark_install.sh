54214a6099f5a627b467546acb1f2da01d95a3ba
woohyeon kim <wh.ururu@gmail.com>
2018-11-25 23:25:02 +0900

    update spark_install.sh

--- hadoop-spark-base/spark_install.sh
+++ hadoop-spark-base/spark_install.sh
@@ -8,5 +8,5 @@ sudo ln -s spark-2.3.1-bin-hadoop2.7 spark
 sudo chown -R ururu:staff /opt/spark
 
 echo -e "\n## spark home" >> ~/.bashrc
-echo -e "export SPARK_HOME=spark-2.3.1-bin-hadoop2.7" >> ~/.bashrc
+echo -e "export SPARK_HOME=/opt/spark" >> ~/.bashrc
 echo -e "export PATH=\${SPARK_HOME}/bin:\$PATH" >> ~/.bashrc
